1913e3b07da9b7aefa1c3ffc46ef96f40b192bcb
laminf <sanyanglamin53@gmail.com>
2021-10-21 14:37:02 +0000

    2.4: Course information step9

--- template/src/App.js
+++ template/src/App.js
@@ -1,81 +1,118 @@
 
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-      name:'Redux',
-      exercises:11,
-      id:4
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
     }
-    ]
-  }
-
+  ]
   return (
     <div>
-   <Course course={course} />
+   <Course courses={courses} />
    
 
   </div>
   )
 }
 
-const Course=({course})=>{
+const Course=({courses})=>{
 
   return(
     <div>
 
-  <Header course={course}/>
-  <Content course={course}/>
-  <Total   course={course}/>
+  <Header courses={courses}/>
+  <Content courses={courses}/>
+  <Headers courses={courses}/>
+  <Contents courses={courses}/>
+  
   
     </div>
   )
 }
-const Header =({course})=>{
+const Header =({courses})=>{
 
   return(
     <div>
-    <h1>{course.name} </h1>
+    <h1>{courses[0].name} </h1>
+    
     </div>
   )
 }
-const Content=({course})=>{
+const Content=({courses})=>{
 
   return(
     <div>
-    <p>{course.parts[0].name} {course.parts[0].exercises} </p>
-    <p>{course.parts[1].name} {course.parts[1].exercises} </p>
-    <p>{course.parts[2].name} {course.parts[2].exercises} </p>
-    <p>{course.parts[3].name} {course.parts[3].exercises} </p>
+    {courses[0].parts.map(item=>{
+      return <div><p>{item.name} {item.exercises}</p> </div>
+    })}
+  total of  {courses[0].parts.reduce((a,i)=>{
+    return a+i.exercises
+  },0)} exercises
     </div>
   )
 }
-const Total =({course})=>{
+const Headers =({courses})=>{
 
   return(
     <div>
-total of {course.parts[0].exercises+course.parts[1]. exercises+course.parts[2].exercises +course.parts[3].exercises} exercises
+    <h1>{courses[1].name} </h1>
+    
     </div>
   )
 }
+const Contents=({courses})=>{
+
+  return(
+    <div>
+    {courses[1].parts.map(item=>{
+      return <div><p>{item.name} {item.exercises}</p> </div>
+    })}
+  total of {courses[1].parts.reduce((a,i)=>{
+    return a+i.exercises
+  },0)} exercises
+    </div>
+  )
+}
+
 export default App
 
 
